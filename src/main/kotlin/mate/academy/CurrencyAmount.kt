556596d0b02251139c2788ca4c1c02839a616362
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2024-03-25 21:48:23 +0200

    Added new lines to the end of the files.

--- src/main/kotlin/mate/academy/CurrencyAmount.kt
+++ src/main/kotlin/mate/academy/CurrencyAmount.kt
@@ -1,8 +1,10 @@
 package mate.academy
 
+private const val MINIMAL_AMOUNT = 0.0
+
 @JvmInline
 value class CurrencyAmount(val amount: Double) {
     init {
-        require(amount >= 0.0)
+        require(amount >= MINIMAL_AMOUNT)
     }
 }
