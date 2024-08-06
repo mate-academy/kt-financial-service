cbc104ceb0f5c922825e11107db3ffe1060517aa
TonyH277 <gajjduktoxa@gmail.com>
2024-08-06 11:34:29 +0300

    implement FinancialService

---
+++ src/main/kotlin/mate/academy/model/CurrencyCode.kt
@@ -0,0 +1,8 @@
+package mate.academy.model
+
+@JvmInline
+value class CurrencyCode(val code: String) {
+    init {
+        require(code.matches(Regex("[A-Z]{3}"))) {"Currency code must be 3 letter uppercase format"}
+    }
+}
