18737e8333608ad25277e080d0c4e76ff4377779
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2024-09-30 20:38:37 +0300

    Fixed a few more bugs

--- src/main/kotlin/mate/academy/model/CurrencyCode.kt
+++ src/main/kotlin/mate/academy/model/CurrencyCode.kt
@@ -1,11 +1,11 @@
 package mate.academy.model
 
-import java.lang.IllegalArgumentException
+private const val DEFAULT_LENGTH = 3
 
 @JvmInline
 value class CurrencyCode(val code: String) {
     init {
-        require(code.length >= 3) { "Currency code must be at least 3 characters long." }
+        require(code.length >= DEFAULT_LENGTH) { "Currency code must be at least 3 characters long." }
         require(code.all { it.isUpperCase() }) { "Currency code must be in uppercase." }
     }
 }
--- src/test/kotlin/mate/academy/FinancialServiceTest.kt
+++ src/test/kotlin/mate/academy/FinancialServiceTest.kt
@@ -4,7 +4,9 @@ import mate.academy.model.AccountNumber
 import mate.academy.model.CurrencyAmount
 import mate.academy.model.CurrencyCode
 import mate.academy.model.TransactionId
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertDoesNotThrow
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.function.Executable
