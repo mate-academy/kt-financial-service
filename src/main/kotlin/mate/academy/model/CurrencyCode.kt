791bb254e2d0c8d0ad26307b65709b09c902e016
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2024-10-03 18:53:29 +0300

    Fixed a bug

--- src/main/kotlin/mate/academy/model/CurrencyCode.kt
+++ src/main/kotlin/mate/academy/model/CurrencyCode.kt
@@ -5,7 +5,7 @@ private const val DEFAULT_LENGTH = 3
 @JvmInline
 value class CurrencyCode(val code: String) {
     init {
-        require(code.length >= DEFAULT_LENGTH) { "Currency code must be at least 3 characters long." }
+        require(code.length == DEFAULT_LENGTH) { "Currency code must be at least 3 characters long." }
         require(code.all { it.isUpperCase() }) { "Currency code must be in uppercase." }
     }
 }
