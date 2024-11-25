82688c26745851d3e7a8793f09fcd40e04f35fc4
oleksii <oleksiiukrainetss@gmail.com>
2024-11-25 20:21:07 +0100

    completed task

---
+++ src/main/kotlin/mate/academy/AccountNumber.kt
@@ -0,0 +1,8 @@
+package mate.academy
+
+@JvmInline
+value class AccountNumber(val number: String) {
+    init {
+        require(number.matches(Regex("^\\d{10}$"))) { "Account number must be a 10-digit string." }
+    }
+}
