eabdd58cebe82e13216e1fab8b5136203a2d0f66
OD <sdenisenko055@gmail.com>
2025-03-21 19:49:14 +0200

    fix

--- src/main/kotlin/mate/academy/AccountNumber.kt
+++ src/main/kotlin/mate/academy/AccountNumber.kt
@@ -3,7 +3,7 @@ package mate.academy
     @JvmInline
     value class AccountNumber(val number : String) {
         init {
-            require(!number.matches(Regex("^\\d{10}\$")),
-                throw IllegalArgumentException("The value does not consist of 10 digits"))
+            require(number.matches(Regex("^\\d{10}\$"))
+            ) { "The value does not consist of 10 digits" }
         }
     }
